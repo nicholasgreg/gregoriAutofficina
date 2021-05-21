d26c0510646c2c75cccb6c8080ad66dd58778ffe
NG <nicholasgregori2003@gmail.com>
2021-05-21 22:38:10 +0200

    creazione commenti javadoc non funziona la creazione del javadoc sistemazione eccezioni

--- src/main/java/com/mycompany/gregoriautofficina/Autofficina.java
+++ src/main/java/com/mycompany/gregoriautofficina/Autofficina.java
@@ -18,21 +18,37 @@ import java.time.DateTimeException;
 import java.util.InputMismatchException;
 
 /**
- *
+ * la classe rappresenta un autofficina
+ * La classe contiene tutti i metodi per effettuare operazioni sugli oggetti 
+ * di classe Revisone, essi sno contenuti in un arrey
+ * i suoi attributi sono:<br>
+ * elencoRevisioni: è un array di oggetti di classe Revisione 
+ * NUM_MAX_REVISIONI: attributo statico,è una costante, rappresenta il numero masiimo delle revisioni che è 100
+ * numeroRevPresenti: è un contatore che conta il numero di revisioni presenti nell'array
  * @author Nicolas
+ * @version 1.0
  */
 public class Autofficina implements Serializable
 {
     private Revisione[] elencoRevisioni;
     private static int NUM_MAX_REVISIONI=100;
     private int numeroRevPresenti=0;
-    
+    /**
+     * Costruttore della classe Autofficina
+     * Consente di istanziare una nuova autofficina
+     * costituito da NUM_MAX_REVISIONI revisioni vuote
+     */
 
     public Autofficina() 
     {
         elencoRevisioni =new Revisione[NUM_MAX_REVISIONI];
     }
-
+    /**
+     * Costruttore di copia. Consente di istanziare una nuova Autofficina,
+     * copiando un altro oggetto di classe Autofficina
+     * copia dell' Autofficina passata come parametro
+     * @param a oggetto di classe Autofficina da cui effettuare la copia
+     */
     public Autofficina(Autofficina a)
     {
         elencoRevisioni=new Revisione[NUM_MAX_REVISIONI];       
@@ -42,18 +58,30 @@ public class Autofficina implements Serializable
             elencoRevisioni[i]=a.getRevisione(i);
         }
     }
-    
+    /**
+     * Metodo getter della costante NUM_MAX_REVISIONI
+     * Ci permette di visualizzare il valore della variabile NUM_MAX_REVISIONI
+     * @return valore NUM_MAX_REVISIONI
+     */
 
     public int getNUM_MAX_REVISIONI() 
     {
         return NUM_MAX_REVISIONI;
     }
-
+    /**
+     * Metodo setter della costante NUM_MAX_REVISIONI
+     * Ci permette di assegnare un valore alla variabile NUM_MAX_REVISIONI 
+     * @param NUM_MAX_REVISIONI valore della variabile NUM_MAX_REVISIONI
+     */
     public static void setNUM_MAX_REVISIONI(int NUM_MAX_REVISIONI) 
     {
         Autofficina.NUM_MAX_REVISIONI = NUM_MAX_REVISIONI;
     }
-
+    /**
+     * Metodo getter della variabile numeroRevPresenti
+     * Ci permette di visualizzare il valore della variabile numeroRevPresenti
+     * @return valore  numeroRevPresenti
+     */
     public int getNumeroRevPresenti() 
     {
         return numeroRevPresenti;
@@ -63,12 +91,22 @@ public class Autofficina implements Serializable
     {
         this.numeroRevPresenti = numeroRevPresenti;
     }*/
-    
+    /**
+     * Metodo getter dell'array di oggetti di classe Revisione elencoRevisioni
+     * inserendo la posizione dell'oggetto che vogliamo visualizzare
+     * Ci permette di visualizzare il contenuto dell'oggetto in posizione posizione
+     * @param posizione è la posizione in cui voglio vedere l'oggetto
+     * @return Il contenuto dell'array elencorevisioni in posizione posizione, sarà un oggetto di classe Revisione
+     */
     public Revisione getRevisione(int posizione)
     {
         return elencoRevisioni[posizione];
     }
-    
+    /**
+     * Metodo getter del numero delle revisioni presenti con valore diverso da null
+     * Ci permette di visualizzare il numero di revisioni presenti nell'array con contenuto diverso da null
+     * @return numero di revisioni presenti nell'array con valore diverso da null
+     */
     public int getNumRevisioni()
     {
         int contatore=0;
@@ -80,7 +118,13 @@ public class Autofficina implements Serializable
         }
         return contatore;
     }
-    
+    /**
+     * Metodo aggiungi revisione all'array
+     * Ci permette di aggiungere una nuova revisione all'array di oggetti di classe Revisione
+     * @param r oggetto di classe Revisione da aggiungere all'array
+     * @return restituisce 0 se l'aggiunta è andata a buon fine
+     * @throws InputMismatchException .
+     */
     
     public int aggiungiRevisione(Revisione r) throws InputMismatchException
     {
@@ -89,17 +133,33 @@ public class Autofficina implements Serializable
             return 0;
             
     }
-    
+    /**
+     * Metodo getter dell'array di oggetti di classe Revisione elencoRevisioni
+     * inserendo la posizione dell'oggetto che vogliamo visualizzare
+     * Ci permette di visualizzare il contenuto dell'oggetto in posizione posizione
+     * @param posizione è la posizione in cui voglio vedere l'oggetto
+     * @return Il contenuto dell'array elencorevisioni in posizione posizione, sarà un oggetto di classe Revisione
+     */
     public Revisione getRevisionePosizione(int posizione)
     {
         return elencoRevisioni[posizione];
     }
-    
+    /**
+     * Metodo che visulizza tutte le revisioni di una targa
+     * Ci permette di visualizzare tuute le revisioni di una targa
+     * @param targa targa di cui cercare le revisioni
+     * @return un array di classe Revisione contenente tutte le revisioni di quella targa
+     * @throws ArrayIndexOutOfBoundsException .
+     * @throws InputMismatchException .
+     * @throws EccezioneValoreNonEsistente  .
+     */
     public Revisione[] revisioniTarga(String targa) throws ArrayIndexOutOfBoundsException,InputMismatchException,EccezioneValoreNonEsistente
     {
         Revisione[] revisioniTarga=new Revisione[getNumRevisioni()];
         Revisione revisione;
         int x=0;
+        
+        int c=0;
         
         for(int i=0;i<getNumRevisioni();i++)
         {
@@ -109,19 +169,33 @@ public class Autofficina implements Serializable
                 revisioniTarga[x]=revisione;
                 x++;
             }
-            else
-                throw new EccezioneValoreNonEsistente();
-                
+            else if(elencoRevisioni[i].getTarga().compareToIgnoreCase(targa)!=0)
+            {
+                c++;
+            }
+            
         }
+        if(c==numeroRevPresenti)
+            throw new EccezioneValoreNonEsistente(); 
+        
         return revisioniTarga;
         
     }
-    
+    /**
+     * Metodo che visulizza tutte le revisioni effettuate in un determinato giorno
+     * Ci permette di visualizzare tuute le revisioni di un determinato giorno,mese,anno
+     * @param data data di cui vuoi visualizzare le revisioni
+     * @return un array di classe Revisione contenente tutte le revisioni del giorno inserito come input
+     * @throws DateTimeException .
+     * @throws InputMismatchException .
+     * @throws EccezioneValoreNonEsistente  .
+     */
     public Revisione[] revisioniGiorno(LocalDate data) throws DateTimeException, InputMismatchException,EccezioneValoreNonEsistente
     {
         Revisione[] revisioniGiorno=new Revisione[getNumRevisioni()];
         Revisione revisione;
         int x=0;
+        int c=0;
         
         for(int i=0;i<getNumRevisioni();i++)
         {
@@ -131,16 +205,30 @@ public class Autofficina implements Serializable
                 revisioniGiorno[x]=revisione;
                 x++;
             }
-            else
-                throw new EccezioneValoreNonEsistente();
-                
+            else if(elencoRevisioni[i].getData().isEqual(data)==false)
+            {
+                c++; 
+            }
         }
+        if(c==numeroRevPresenti)
+            throw new EccezioneValoreNonEsistente(); 
         return revisioniGiorno;
           
     }
-    
+    /**
+     * Metodo che elimina una revisione inserendo il codice identificativo della revisione
+     * ci permette di eliminare una revisione dall'array elencoRevisioni
+     * @param codice codice della revisione che vogliamo eliminare
+     * @return restituisce: 
+     *                      0: se l'eliminazione è andata a buon fine
+     *                     -1: se l'eliminazione non è andata a buon fine
+     * @throws InputMismatchException .
+     * @throws EccezioneValoreNonEsistente  .
+     */
     public int eliminaRevisione(int codice) throws InputMismatchException,EccezioneValoreNonEsistente
     {
+        int c=0;
+        
         for(int i=0;i<numeroRevPresenti;i++)
             {
                 if (elencoRevisioni[i]!=null)
@@ -150,11 +238,14 @@ public class Autofficina implements Serializable
                         aggiornaPosizioneRevisione(i);
                         return 0; 
                     }
-                    else
-                      throw new EccezioneValoreNonEsistente();
-                    
+                    else if(elencoRevisioni[i].getCodiceId()!=codice)
+                    {
+                        c++;
+                    }
                 }
             }
+        if(c==numeroRevPresenti)
+            throw new EccezioneValoreNonEsistente(); 
         return -1;
         
         /*Revisione r;
@@ -173,7 +264,12 @@ public class Autofficina implements Serializable
         }
         return -1;*/
     }
-    
+    /**
+     * Metodo che aggiorna la posizione degli oggetti nell'array dopo l'eliminazione di un oggetto
+     * Inserendo la posizione ci permette di spostare gli oggetti
+     * per non lasciare il buco creato dall'eliminazione
+     * @param posizione posizione in cui deve spostare gli elementi
+     */
     private void aggiornaPosizioneRevisione(int posizione)
     {
         for (int i=posizione;i<numeroRevPresenti-1;i++)  
@@ -183,12 +279,23 @@ public class Autofficina implements Serializable
         elencoRevisioni[numeroRevPresenti-1]=null;      //se ne elimini uno, sicuramente l'ultimo deve contenere null ( lo faccio per togliere dall'array la revisione eliminata)
         numeroRevPresenti--;
     }
-    
+    /**
+     * Metodo che visualizza le revisioni effettuate sull'auto di una persona inserendo nome e cognome del cliente
+     * e che ordina in ordine dal più recente al più vecchio le revisioni in base alla data
+     * @param nome nome della persona cui vogliamo visualizzare le revisioni
+     * @param cognome cognome della persona cui vogliamo visualizzare le revisioni
+     * @return restituisce un array di oggetti di classe revisione contenente 
+     * le revisioni di una persona ordinate dalla più recente alla più vecchia le revisioni in base alla data
+     * @throws NullPointerException .
+     * @throws ArrayIndexOutOfBoundsException .
+     * @throws EccezioneValoreNonEsistente  .
+     */
     public Revisione[] InterventiAutoPersonaDecrescente(String nome,String cognome) throws NullPointerException,ArrayIndexOutOfBoundsException,EccezioneValoreNonEsistente
     {
         Revisione[] interventiAutoDecresc=new Revisione[getNumeroRevPresenti()];
         Revisione revisione;
         int x=0;
+        int c=0;
         
         for(int i=0;i<numeroRevPresenti;i++)
         {
@@ -198,13 +305,24 @@ public class Autofficina implements Serializable
                 interventiAutoDecresc[x]=revisione;
                 x++;
             }
-            else
-                throw new EccezioneValoreNonEsistente();
+            else if(elencoRevisioni[i].getNome().compareToIgnoreCase(nome)!=0 && elencoRevisioni[i].getCognome().compareToIgnoreCase(cognome)!=0)
+            {
+                c++;
+            }
         }
+        if(c==numeroRevPresenti)
+            throw new EccezioneValoreNonEsistente();
+        
         interventiAutoDecresc=Ordinatore.selectionSortAutoPersonaDecrescente(interventiAutoDecresc);
         return interventiAutoDecresc;
     }
-            
+    /**
+     * Metodo che salva tutte le revisioni dell'array su un file CSV
+     * @param nomeFile nome del file CSV su cui vogliamo salvare i dati
+     * @throws IOException .
+     * @throws EccezioniPosizioneNonValida .
+     * @throws FileException  .
+     */      
     public void salvaRevisione(String nomeFile) throws IOException,EccezioniPosizioneNonValida, FileException
     {
         TextFile f1=new TextFile(nomeFile,'W');
@@ -219,7 +337,13 @@ public class Autofficina implements Serializable
             }
         f1.close();
     }
-    
+    /**
+     * Metodo che salva tutte le revisioni dell'array su un file binario
+     * @param nomeFile nome del file binario su cui vogliamo salvare i dati
+     * @throws IOException .
+     * @throws EccezioniPosizioneNonValida .
+     * @throws FileException  .
+     */
     public void salvaRevisioneBin(String nomeFile) throws IOException,EccezioniPosizioneNonValida, FileException
     {
         FileOutputStream f1=new FileOutputStream(nomeFile);
--- src/main/java/com/mycompany/gregoriautofficina/Revisione.java
+++ src/main/java/com/mycompany/gregoriautofficina/Revisione.java
@@ -10,8 +10,19 @@ import java.time.LocalDate;
 import java.time.Month;
 
 /**
- *
+ * la classe rappresenta una revisione su un auto
+ * i suoi attributi sono:<br>
+ * codiceId: codice identificativo della revisione<br>
+ * targa: targa del mezzo da revisionare<br>
+ * nome: nome del proprietario del mezzo da revisionare<br>
+ * cognome: cognome del proprietario del mezzo da revisionare<br>
+ * descrizioneIntervento: breve descrizione dell'intervento che si andrà ad effettuare<br>
+ * data: data in cui viene fatta la revisione<br>
+ * costo: costo dell'intervento<br>
+ * In questa classe vengono inserite tutte le informazioni
+ * di una revisione.
  * @author Nicolas
+ * @version 1.0
  */
 public class Revisione implements Serializable
 {
@@ -23,6 +34,19 @@ public class Revisione implements Serializable
     private LocalDate data;
     private float costo;
 
+    /**
+     * Costruttore della classe Revisione 
+     * Consente di istanziare una nuova revisione
+     * @param codiceId: codice identificativo della revisione
+     * @param targa: targa del mezzo da revisionare
+     * @param nome: nome del proprietario del mezzo da revisionare
+     * @param cognome: cognome del proprietario del mezzo da revisionare
+     * @param descrizioneIntervento: breve descrizione dell'intervento che si andrà ad effettuare
+     * @param costo: costo dell'intervento
+     * @param anno: anno in cui viene effettuata la revisone
+     * @param mese: mese in cui viene effettuata la revisone
+     * @param giorno: giorno in cui viene effettuata la revisone
+     */
     public Revisione(int codiceId, String targa, String nome, String cognome, String descrizioneIntervento, float costo,int anno,int mese,int giorno) 
     {
         this.codiceId = codiceId;
@@ -33,7 +57,11 @@ public class Revisione implements Serializable
         data=LocalDate.of(anno,mese,giorno);
         this.costo = costo;
     }
-    
+    /**
+     * Costruttore di copia della classe Revisione. consente di istanziare una nuova revisione
+     * copia della revisione passata come parametro
+     * @param r Oggetto di classe Revisione di cui verrà effettuata la copia nel costruttore di copia
+     */
     public Revisione(Revisione r)
     {
         codiceId=r.getCodiceId();
@@ -44,7 +72,18 @@ public class Revisione implements Serializable
         data=r.getData();
         costo=r.getCosto();
     }
-    
+    /**
+     * Costruttore di default della classe Revisione
+     * Consente di istanziare un oggetto di classe Revsione con valori nulli 
+     * alle variabili
+     * codiceId: con valore 0
+     * targa: come stringa vuota
+     * nome: come stringa vuota
+     * cognome: come stringa vuota
+     * descrizioneIntervento: come stringa vuota
+     * data: con valore del giorno,mese e anno e momento in cui viene istanziata
+     * costo: con valore 0
+     */
     public Revisione()
     {
         codiceId=0;
@@ -56,77 +95,148 @@ public class Revisione implements Serializable
         costo=0;
     }
 
+    /**
+     * Metodo getter della variabile CodiceId
+     * Ci permette di visualizzare il valore della variabile codice identificativo
+     * @return codiceId
+     */
     public int getCodiceId() 
     {
         return codiceId;
     }
 
+    /**
+     * Metodo getter della variabile Targa
+     * Ci permette di visualizzare il contenuto della variabile Targa
+     * @return targa
+     */
     public String getTarga() 
     {
         return targa;
     }
 
+    /**
+     * Metodo getter della variabile Nome
+     * Ci permette di visualizzare il contenuto della variabile nome
+     * @return nome
+     */
     public String getNome() 
     {
         return nome;
     }
 
+    /**
+     * Metodo getter della variabile cognome
+     * Ci permette di visualizzare il contenuto della variabile cognome
+     * @return cognome
+     */
     public String getCognome()
     {
         return cognome;
     }
 
+    /**
+     * Metodo getter della variabile descrizioneIntervento
+     * Ci permette di visualizzare il contenuto della variabile descrizioneIntervento
+     * @return descrizioneIntervento
+     */
     public String getDescrizioneIntervento() 
     {
         return descrizioneIntervento;
     }
 
+    /**
+     * Metodo getter della variabile data
+     * Ci permette di visualizzare il contenuto della variabile data
+     * @return data
+     */
     public LocalDate getData()
     {
         return data;
     }
-
+    /**
+     * Metodo getter della variabile costo
+     * Ci permette di visualizzare il contenuto della variabile costo
+     * @return costo
+     */
     
     public float getCosto()
     {
         return costo;
     }
 
+    /**
+     * Metodo setter della variabile codiceId
+     * Ci permette di assegnare un valore alla variabile codiceId
+     * @param codiceId valore della variabile codiceId
+     */
     public void setCodiceId(int codiceId)
     {
         this.codiceId = codiceId;
     }
-
+    /**
+     * Metodo setter della variabile targa
+     * Ci permette di assegnare un valore alla variabile targa
+     * @param targa valore della variabile targa
+     */
     public void setTarga(String targa)
     {
         this.targa = targa;
     }
-
+    /**
+     * Metodo setter della variabile nome
+     * Ci permette di assegnare un valore alla variabile nome
+     * @param nome valore della variabile nome
+     */
     public void setNome(String nome)
     {
         this.nome = nome;
     }
-
+    /**
+     * Metodo setter della variabile cognome
+     * Ci permette di assegnare un valore alla variabile cognome
+     * @param cognome valore della variabile cognome
+     */
     public void setCognome(String cognome)
     {
         this.cognome = cognome;
     }
-
+    /**
+     * Metodo setter della variabile descrizioneIntervento
+     * Ci permette di assegnare un valore alla variabile descrizioneIntervento
+     * @param descrizioneIntervento valore della variabile descrizioneIntervento
+     */
     public void setDescrizioneIntervento(String descrizioneIntervento) 
     {
         this.descrizioneIntervento = descrizioneIntervento;
     }
-
+    /**
+     * Metodo setter della variabile data
+     * Ci permette di assegnare un valore alla variabile data inserendo il valore di anno,mese e giorno.
+     * @param anno valore della variabile anno
+     * @param mese valore della variabile mese
+     * @param giorno valore della variabile giorno
+     */
     public void setData(int anno,int mese,int giorno) 
     {
         data=LocalDate.of(anno,mese,giorno);
     }
 
+    /**
+     * Metodo setter della variabile costo
+     * Ci permette di assegnare un valore alla variabile costo
+     * @param costo valore della variabile costo
+     */
     public void setCosto(int costo) 
     {
         this.costo = costo;
     }
 
+    /**
+     * Metodo toString di tutti gli attributi della classe Revisone, degli oggetti di classe Revsione
+     * Ci permette di visualizzare il valore di tutti gli attributi dell'oggetto di classe Revisione
+     * @return una stringa contenente il valore deglia attributi di revisione
+     */
     @Override
     public String toString() 
     {
